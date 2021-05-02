227ea688932c6d74e259cac54c974b6681850c7a
moses1206 <mose23@naver.com>
2021-05-02 22:44:07 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
+import User from "./components/users/User";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <div className="container">
+          <User loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
