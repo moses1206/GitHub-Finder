7787d0df4d1d0587774a563bfcd299ec362fd139
moses1206 <mose23@naver.com>
2021-05-03 15:15:23 +0900

    2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 import Navbar from "./components/layout/Navbar";
 import User from "./components/users/User";
+import Search from "./components/users/Search";
 
 class App extends Component {
   state = {
@@ -11,18 +12,22 @@ class App extends Component {
     loading: false,
   };
 
-  async componentDidMount() {
+  // Search Github users
+  searchUsers = async (text) => {
     this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
-    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data.items, loading: false });
+  };
 
-    this.setState({ users: res.data, loading: false });
-  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <User loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
---
+++ src/components/users/Search.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const Search = ({ searchUsers }) => {
+  const [text, setText] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    searchUsers(text);
+    setText("");
+  };
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users ..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+    </div>
+  );
+};
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+};
+
+export default Search;
